4bc2d568cb80862f564fde2c8c5a819d44ec7b05
gat60 <namanau7@gmail.com>
2025-11-30 23:04:19 +0530

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+iris = datasets.load_iris()
+print(iris.target_names)
+
+x, y = datasets.load_iris(return_x_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
+
+data = pd.DataFrame({
+    'sepalength': iris.data[:, 0],
+    'sepalwidth': iris.data[:, 1],
+    'petallength': iris.data[:, 2],
+    'petalwidth': iris.data[:, 3],
+    'species': iris.target
+})
+print(data.head())
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+print("Accuracy of the model:", metrics.accuracy_score(y_test, y_pred))
+
+print(clf.predict([[3, 3, 2, 2]]))
+print(iris.target_names[clf.predict([[3, 3, 2, 2]])])
